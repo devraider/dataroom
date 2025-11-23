2d6fe50a603e72b5124d1a8c8a6e61e094db6a96
crtpaul <cristeapaul.2015@gmail.com>
2025-11-23 13:42:50 +0200

    fe: feat:  ThemeToggle - useState for theme selection

--- frontend/src/layout/ThemeToggle.tsx
+++ frontend/src/layout/ThemeToggle.tsx
@@ -1,14 +1,30 @@
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+
+enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+  SYSTEM = "system",
+}
 
 export default function ThemeToggle() {
+  const [theme, setTheme] = useState(Theme.SYSTEM);
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>Theme</DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem>Light</DropdownMenuItem>
         <DropdownMenuItem>Dark</DropdownMenuItem>
