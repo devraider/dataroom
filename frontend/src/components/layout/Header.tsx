8b423a9426a4f44f3414635729cbeee5befd7aaf
crtpaul <cristeapaul.2015@gmail.com>
2025-11-23 22:36:59 +0200

    fe: feat: UserMenu: used in Header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,7 +1,19 @@
-import { Folder } from "lucide-react";
+import { Folder, User } from "lucide-react";
 import ThemeToggle from "./ThemeToggle";
+import UserMenu from "./UserMenu";
+import { UserRole } from "@/types/auth";
 
 export default function Header() {
+  const user = {
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    role: UserRole.ADMIN,
+    picture: "https://placehold.co/600x400/png?text=Hello+World",
+  };
+  const handleLogout = () => {
+    // Implement logout functionality here
+  };
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-16 items-center justify-between px-4 w-full">
@@ -12,6 +24,7 @@ export default function Header() {
 
         <div className="flex items-center gap-2">
           <ThemeToggle />
+          <UserMenu user={user} onLogout={handleLogout} />
         </div>
       </div>
     </header>
