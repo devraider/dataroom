1e1e690d6a98aad2b8091310ef705259e625ed93
crtpaul <cristeapaul.2015@gmail.com>
2025-11-23 13:39:37 +0200

    fe: feat:  ThemeToggle - added buttons

---
+++ frontend/src/layout/ThemeToggle.tsx
@@ -0,0 +1,19 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function ThemeToggle() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>Theme</DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem>Light</DropdownMenuItem>
+        <DropdownMenuItem>Dark</DropdownMenuItem>
+        <DropdownMenuItem>System</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
