03cbddad3b1e8cb0f3b2f81772aaffc0d5f02422
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 17:21:40 +0200

    fe: feat: WorkspaceList:  return spinner in case of loading state

--- frontend/src/components/workspaces/WorkspaceList.tsx
+++ frontend/src/components/workspaces/WorkspaceList.tsx
@@ -1,9 +1,29 @@
+import { workspaceService } from "@/services/workspaceService";
+import { useWorkspaceStore } from "@/store/workspaceStore";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import Layout from "../layout/Layout";
+import { LoadingSpinner } from "../common/LoadingSpinner";
+
 export default function WorkspaceList() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const setCurrentWorkspace = useWorkspaceStore(
     (state) => state.setCurrentWorkspace
   );
+
+  const { data: workspaces, isLoading } = useQuery({
+    queryKey: ["workspaces"],
+    queryFn: workspaceService.getAll,
+  });
+
+  if (isLoading) {
+    return (
+      <Layout>
+        <LoadingSpinner />
+      </Layout>
+    );
+  }
 
   return <div>Workspace List Component</div>;
 }
