c6dddbe53bbc7775c8ee4aba497ea1988fc51199
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 20:00:51 +0200

    be: feat: boostrap app with settings

--- backend/src/asgi.py
+++ backend/src/asgi.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+
 import uvicorn
+from fastapi import FastAPI
+
+from backend.src.utils.settings import settings
 
 app = FastAPI(title="Data Room", version="1.0.0")
 
 
 if __name__ == "__main__":
-
-    uvicorn.run(app)
+    uvicorn.run(app, host=settings.HOST, port=settings.PORT, reload=settings.DEBUG)
