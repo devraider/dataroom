725dbb10e81cc591c01baa420c565b7987531bb0
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 22:58:05 +0200

    be: feat: add relation between User and Workspace

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,7 +1,7 @@
 import datetime as dt
 from typing import TYPE_CHECKING, Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 from backend.src.types.date import utc_now
 
@@ -22,3 +22,6 @@ class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     created_at: dt.datetime = Field(default_factory=utc_now)
     updated_at: dt.datetime = Field(default_factory=utc_now)
+    
+    # Relationships
+    workspaces: list["WorkspaceMember"] = Relationship(back_populates="user")
