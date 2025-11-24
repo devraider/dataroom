14983946e4bd94bbbbb88f51af6f7cf036954cf8
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 23:23:40 +0200

    be: feat: use schema response WorkspaceResponse

--- backend/src/api/routes/workspace.py
+++ backend/src/api/routes/workspace.py
@@ -1,15 +1,15 @@
-from typing import Any
 
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
 from backend.src.database.session import get_session
 from backend.src.models.workspace import Workspace
+from backend.src.schemas.workspace import WorkspaceResponse
 
 workspace_router = APIRouter(prefix="/workspaces", tags=["workspaces"])
 
 
-@workspace_router.get("/", response_model=list[Any])
+@workspace_router.get("/", response_model=list[WorkspaceResponse])
 def get_workspaces(
     session: Session = Depends(get_session),
     skip: int = 0,
