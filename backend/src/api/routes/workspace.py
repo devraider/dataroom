5a3361c2f1ef058090eb6a1176a863de2eddbcb9
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 22:37:47 +0200

    be: feat: defined workspace_router and first endpoint to get all workspaces

---
+++ backend/src/api/routes/workspace.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from fastapi import APIRouter, Depends
+from sqlmodel import Session, select
+
+from backend.src.database.session import get_session
+from backend.src.models.workspace import Workspace
+
+workspace_router = APIRouter(prefix="/workspaces", tags=["workspaces"])
+
+
+@workspace_router.get("/", response_model=list[Any])
+def get_workspaces(
+    session: Session = Depends(get_session),
+    skip: int = 0,
+    limit: int = 100,
+):
+    """Get all workspaces"""
+    statement = select(Workspace)
+    workspaces = session.exec(statement).offset(skip).limit(limit).all()
+
+    return workspaces
