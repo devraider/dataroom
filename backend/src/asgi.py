deb1fa810e48a4599b8357a3a7efae32884de139
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 22:37:26 +0200

    be: feat: registered workspace_router

--- backend/src/asgi.py
+++ backend/src/asgi.py
@@ -1,5 +1,10 @@
 
 from fastapi import FastAPI
 
+from backend.src.api.routes.workspace import workspace_router
+
 app = FastAPI(title="Data Room", version="1.0.0")
 
+
+
+app.include_router(workspace_router)
