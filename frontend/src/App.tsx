79ffbc1e5cfe155675db2fd2af8a80befbb284cd
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 16:37:18 +0200

    fe: feat: App: render DemoLogin conditionally based on isDemoMode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import { Toaster } from "./components/ui/sonner";
 import DemoLoginPage from "./components/layout/DemoLoginPage";
+import { isDemoMode } from "./lib/config";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<DemoLoginPage />} />
+        <Route
+          path="/login"
+          element={isDemoMode() ? <DemoLoginPage /> : null}
+        />
         <Route element={<ProtectedRoute />}>
           <Route
             path="/workspaces/:workspaceId/files"
