f4fa49f4de2ead667ae5710a6d74d23741ce1384
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 20:51:28 +0200

    be: feat: cleand asgi.py helper code

--- backend/src/asgi.py
+++ backend/src/asgi.py
@@ -1,11 +1,5 @@
 
-import uvicorn
 from fastapi import FastAPI
-
-from backend.src.utils.settings import settings
 
 app = FastAPI(title="Data Room", version="1.0.0")
 
-
-if __name__ == "__main__":
-    uvicorn.run(app, host=settings.HOST, port=settings.PORT, reload=settings.DEBUG)
