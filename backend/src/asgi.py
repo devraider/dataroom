a92369ab6c01f97b9a6804588dd7f925fde0a579
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 22:42:28 +0200

    be: feat: created lifespan for FastAPI app to created table is not exist

--- backend/src/asgi.py
+++ backend/src/asgi.py
@@ -1,9 +1,18 @@
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from backend.src.api.routes.workspace import workspace_router
 
-app = FastAPI(title="Data Room", version="1.0.0")
+
+@asynccontextmanager
+async def lifespan_event(app: FastAPI):
+    print("Starting up...")
+    yield
+    print("Shutting down...")
+
+
+app = FastAPI(title="Data Room", version="1.0.0", lifespan=lifespan_event)
 
 
 
