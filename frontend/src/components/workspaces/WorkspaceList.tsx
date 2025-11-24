f221e277a7723a50c2caf74ae6ed946294aa3955
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 17:20:22 +0200

    fe: feat: WorkspaceList:  added constants for navigation, queryClient, and setCurrentWorkspace

--- frontend/src/components/workspaces/WorkspaceList.tsx
+++ frontend/src/components/workspaces/WorkspaceList.tsx
@@ -1,3 +1,9 @@
 export default function WorkspaceList() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const setCurrentWorkspace = useWorkspaceStore(
+    (state) => state.setCurrentWorkspace
+  );
+
   return <div>Workspace List Component</div>;
 }
