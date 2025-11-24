cd978bcb76a889ebf8a93e3a9158261271c2dbec
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 18:23:10 +0200

    fe: feat: stub CreateWorkSpaceDialog

---
+++ frontend/src/components/workspaces/CreateWorkspaceDialog.tsx
@@ -0,0 +1,9 @@
+interface CreateWorkspaceDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export default function CreateWorkspaceDialog({
+  open,
+  onOpenChange,
+}: CreateWorkspaceDialogProps) {}
