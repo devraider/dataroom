108814ee081f5103c4d4e660a94dac18b6a90051
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 22:17:26 +0200

    be: feat: added workspace model

--- backend/src/models/workspace.py
+++ backend/src/models/workspace.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
+
+from backend.src.types.date import utc_now
 
 
 class WorkspaceBase(SQLModel):
@@ -8,3 +11,12 @@ class WorkspaceBase(SQLModel):
     name: str = Field(max_length=255)
     description: Optional[str] = Field(default=None, max_length=1000)
 
+
+class Workspace(WorkspaceBase, table=True):
+    """Workspace database model"""
+    __tablename__ = "workspaces"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default_factory=utc_now)
+    updated_at: datetime = Field(default_factory=utc_now)
+    created_by: int = Field(foreign_key="users.id")
