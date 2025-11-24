48663831acbcbaaf3146b2609f350cfd406337aa
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 21:43:28 +0200

    be: feat: added AppSetting object

--- backend/src/utils/settings.py
+++ backend/src/utils/settings.py
@@ -18,4 +18,9 @@ class DatabaseSettings(BaseSettings):
         return f"postgresql+psycopg2://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}"
 
 
+class AppSettings(BaseSettings):
+    DEBUG: bool = False
+    SECRET_KEY: SecretStr
+
 database_settings = DatabaseSettings()
+app_settings = AppSettings()
