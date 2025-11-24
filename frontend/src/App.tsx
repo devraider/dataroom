e9af4ffbae7c25710533aaff99b673ae270ba476
crtpaul <cristeapaul.2015@gmail.com>
2025-11-24 16:36:27 +0200

    fe: feat: DemoLoginPage: used in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import FileList from "./components/files/FileList";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import { Toaster } from "./components/ui/sonner";
+import DemoLoginPage from "./components/layout/DemoLoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<DemoLoginPage />} />
         <Route element={<ProtectedRoute />}>
           <Route
             path="/workspaces/:workspaceId/files"
