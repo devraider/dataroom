7deb442506c2c263fedc82cc4bc278fc43884711
crtpaul <cristeapaul.2015@gmail.com>
2025-11-25 17:39:02 +0200

    be: feat: add ALLOWED_ORIGINS to CORSMiddleware

--- backend/src/asgi.py
+++ backend/src/asgi.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.src.api.routes.workspace import workspace_router
+from backend.src.config.settings import app_settings
 from backend.src.database.session import create_db_and_tables
 
 # Import models so SQLModel knows about them
@@ -21,6 +23,14 @@ async def lifespan_event(app: FastAPI):
 
 app = FastAPI(title="Data Room", version="1.0.0", lifespan=lifespan_event)
 
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=app_settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(workspace_router)
