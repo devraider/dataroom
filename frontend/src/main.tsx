d933a012a79f00868ebd7dd7616271cf6aa3f93a
crtpaul <cristeapaul.2015@gmail.com>
2025-11-26 23:00:31 +0200

    fe: feat: add GoogleOAuthProvider to the app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import App from "./App.tsx";
 import { queryClient } from "./lib/queryClient.ts";
+import { CONFIG } from "./lib/config.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <GoogleOAuthProvider clientId={CONFIG.GOOGLE_CLIENT_ID || ""}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
