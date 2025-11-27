b239abb4edd58f7b19b4e028bef3e47d7373426e
crtpaul <cristeapaul.2015@gmail.com>
2025-11-27 21:11:44 +0200

    be: feat: added TokenBlacklist

---
+++ backend/src/models/token_blacklist.py
@@ -0,0 +1,17 @@
+import datetime as dt
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+from backend.src.types.date import utc_now
+
+
+class TokenBlacklist(SQLModel, table=True):
+    """Token blacklist model for invalidated JWT tokens"""
+    __tablename__ = "token_blacklist"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    token: str = Field(unique=True, index=True, max_length=1024)
+    user_id: int = Field(foreign_key="users.id", index=True)
+    blacklisted_at: dt.datetime = Field(default_factory=utc_now)
+    expires_at: dt.datetime = Field(index=True)
